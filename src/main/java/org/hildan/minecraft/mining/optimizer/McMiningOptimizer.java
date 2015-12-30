bb8492a55bff2244b5c941eed9da38cb799dd9c5
Joffrey BION <joffrey.bion@gmail.com>
2015-12-30 21:52:30 +0100

    collapsed chunk classes into one

--- src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
+++ src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
@@ -1,10 +1,9 @@
 package org.hildan.minecraft.mining.optimizer;
 
 import org.hildan.minecraft.mining.optimizer.chunks.Chunk;
-import org.hildan.minecraft.mining.optimizer.chunks.DugChunk;
-import org.hildan.minecraft.mining.optimizer.chunks.OredChunk;
+import org.hildan.minecraft.mining.optimizer.ore.OreGenerator;
 import org.hildan.minecraft.mining.optimizer.patterns.DigEverythingPattern;
-import org.hildan.minecraft.mining.optimizer.patterns.Pattern;
+import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern;
 
 public class McMiningOptimizer {
 
@@ -14,29 +13,32 @@ public class McMiningOptimizer {
 
         Chunk baseChunk = new Chunk(16, 16, 16);
         OreGenerator oreGenerator = new OreGenerator();
-        Pattern pattern = new DigEverythingPattern();
+        DiggingPattern pattern = new DigEverythingPattern();
 
         long totalOres = 0;
         long foundOres = 0;
         long dugBlocks = 0;
+
         for (int i = 0; i < ITERATIONS; i++) {
-            OredChunk oredChunk = oreGenerator.generate(baseChunk);
-            long initialOres = oredChunk.getNumberOfOres();
+            Chunk oredChunk = oreGenerator.generate(baseChunk);
+            long initialOres = oredChunk.getOresCount();
             totalOres += initialOres;
 
-            DugChunk dugChunk = pattern.dig(oredChunk);
+            Chunk dugChunk = pattern.dig(oredChunk);
             dugBlocks += dugChunk.getNumberOfDugBlocks();
-            foundOres += initialOres - dugChunk.getNumberOfOres();
+            foundOres += initialOres - dugChunk.getOresCount();
         }
+
         double avgTotalOres = (double) totalOres / ITERATIONS;
         double avgFoundOres = (double) foundOres / ITERATIONS;
         double avgDugBlocks = (double) dugBlocks / ITERATIONS;
         double efficiency = (double) foundOres * 100 / dugBlocks;
         double thoroughness = (double) foundOres * 100 / totalOres;
+
         System.out.format("            %9s  %9s%n", "Avg/chunk", "Total");
-        System.out.format("Total ores: %2.6f  %9d%n", avgTotalOres, totalOres);
-        System.out.format("Found ores: %2.6f  %9d%n", avgFoundOres, foundOres);
-        System.out.format("Dug Blocks: %2.6f  %9d%n%n", avgDugBlocks, dugBlocks);
+        System.out.format("Total ores: %6.2f  %9d%n", avgTotalOres, totalOres);
+        System.out.format("Found ores: %6.2f  %9d%n", avgFoundOres, foundOres);
+        System.out.format("Dug Blocks: %6.2f  %9d%n%n", avgDugBlocks, dugBlocks);
         System.out.format("Efficiency:   %3.2f%%%n", efficiency);
         System.out.format("Thoroughness: %3.2f%%%n", thoroughness);
     }
--- src/main/java/org/hildan/minecraft/mining/optimizer/chunks/Chunk.java
+++ src/main/java/org/hildan/minecraft/mining/optimizer/chunks/Chunk.java
@@ -1,5 +1,7 @@
 package org.hildan.minecraft.mining.optimizer.chunks;
 
+import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern;
+
 import java.util.Arrays;
 import java.util.function.Predicate;
 
@@ -82,11 +84,11 @@ public class Chunk {
         return index;
     }
 
-    protected void setBlock(int x, int y, int z, Block block) {
+    public void setBlock(int x, int y, int z, Block block) {
         blocks[getIndex(x, y, z)] = block;
     }
 
-    protected Block getBlock(int x, int y, int z) {
+    public Block getBlock(int x, int y, int z) {
         return blocks[getIndex(x, y, z)];
     }
 
@@ -102,5 +104,73 @@ public class Chunk {
             }
         }
         return count;
+    }
+
+    public long getOresCount() {
+        return countBlocksMatching(Block::isOre);
+    }
+
+    public void dig(int x, int y, int z) {
+        setBlock(x, y, z, Block.AIR);
+    }
+
+    public boolean isDug(int x, int y, int z) {
+        return getBlock(x, y, z) == Block.AIR;
+    }
+
+    protected static int[] getAdjacentIndexes(int i, int size) {
+        if (i == 0) {
+            return new int[] { size - 1, i, i + 1 }; // wrapped max
+        }
+        if (i == size - 1) {
+            return new int[] { i - 1, i, 0 }; // wrapped min
+        }
+        return new int[] { i - 1, i, i + 1 };
+    }
+
+    public boolean hasDugNeighbor(int x, int y, int z) {
+        int[] adjXs = getAdjacentIndexes(x, getWidth());
+        int[] adjYs = getAdjacentIndexes(y, getHeight());
+        int[] adjZs = getAdjacentIndexes(z, getLength());
+        for (int adjX : adjXs) {
+            for (int adjY : adjYs) {
+                for (int adjZ : adjZs) {
+                    if (x == adjX && y == adjY && z == adjZ) {
+                        // skip self
+                        continue;
+                    }
+                    if (isDug(adjX, adjY, adjZ)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isDiscovered(int x, int y, int z) {
+
+        // TODO implement visibility algo
+
+        return isDug(x, y, z) || hasDugNeighbor(x, y, z);
+    }
+
+    /**
+     * Returns whether this chunk can indeed be dug like this in game.
+     *
+     * @param accesses
+     *     the coordinates where the player enters this chunk to start digging, as specified by {@link DiggingPattern#getAccesses()}
+     *
+     * @return true if this chunk can indeed be dug like this in game.
+     */
+    public boolean isValid(int[][] accesses) {
+
+        // TODO check whether the dug blocks are arranged in a way that could indeed have been dug
+
+        return false;
+    }
+
+    public long getNumberOfDugBlocks() {
+        return countBlocksMatching(b -> b == Block.AIR);
     }
 }
