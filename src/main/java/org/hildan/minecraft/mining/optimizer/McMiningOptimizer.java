3abd2e0f8428d5c4c0ae9ee65f3d3e65be64010d
Joffrey BION <joffrey.bion@gmail.com>
2015-12-30 22:15:26 +0100

    improved stat formatting and added exec time

--- src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
+++ src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
@@ -7,7 +7,7 @@ import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern;
 
 public class McMiningOptimizer {
 
-    private static final int ITERATIONS = 500;
+    private static final int ITERATIONS = 10000;
 
     public static void main(String[] args) {
 
@@ -19,6 +19,8 @@ public class McMiningOptimizer {
         long foundOres = 0;
         long dugBlocks = 0;
 
+        long startTime = System.currentTimeMillis();
+
         for (int i = 0; i < ITERATIONS; i++) {
             Chunk oredChunk = oreGenerator.generate(baseChunk);
             long initialOres = oredChunk.getOresCount();
@@ -29,18 +31,23 @@ public class McMiningOptimizer {
             foundOres += initialOres - dugChunk.getOresCount();
         }
 
+        long endTime = System.currentTimeMillis();
+
         double avgTotalOres = (double) totalOres / ITERATIONS;
         double avgFoundOres = (double) foundOres / ITERATIONS;
         double avgDugBlocks = (double) dugBlocks / ITERATIONS;
         double efficiency = (double) foundOres * 100 / dugBlocks;
         double thoroughness = (double) foundOres * 100 / totalOres;
 
-        System.out.format("            %9s  %9s%n", "Avg/chunk", "Total");
-        System.out.format("Total ores: %6.2f  %9d%n", avgTotalOres, totalOres);
-        System.out.format("Found ores: %6.2f  %9d%n", avgFoundOres, foundOres);
-        System.out.format("Dug Blocks: %6.2f  %9d%n%n", avgDugBlocks, dugBlocks);
-        System.out.format("Efficiency:   %3.2f%%%n", efficiency);
-        System.out.format("Thoroughness: %3.2f%%%n", thoroughness);
+        System.out.format("            %10s  %12s%n", "Avg/chunk", "Total");
+        System.out.format("Total ores: %10.2f  %,12d%n", avgTotalOres, totalOres);
+        System.out.format("Found ores: %10.2f  %,12d%n", avgFoundOres, foundOres);
+        System.out.format("Dug Blocks: %10.2f  %,12d%n", avgDugBlocks, dugBlocks);
+        System.out.println();
+        System.out.format("Efficiency:    %5.2f%%%n", efficiency);
+        System.out.format("Thoroughness:  %5.2f%%%n", thoroughness);
+        System.out.println();
+        System.out.format("Exec. time: %d ms", endTime - startTime);
     }
 
 }
