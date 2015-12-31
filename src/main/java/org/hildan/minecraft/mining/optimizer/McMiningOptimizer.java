269356a7856cdcec079ca222dc7ca17e1426161a
Joffrey BION <joffrey.bion@gmail.com>
2015-12-31 21:56:12 +0100

    added more pattern stats in the main()

--- src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
+++ src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
@@ -2,19 +2,34 @@ package org.hildan.minecraft.mining.optimizer;
 
 import org.hildan.minecraft.mining.optimizer.chunks.Chunk;
 import org.hildan.minecraft.mining.optimizer.ore.OreGenerator;
+import org.hildan.minecraft.mining.optimizer.patterns.BranchingPattern;
 import org.hildan.minecraft.mining.optimizer.patterns.DigEverythingPattern;
 import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern;
+import org.hildan.minecraft.mining.optimizer.patterns.tunnels.TunnelPattern;
+import org.hildan.minecraft.mining.optimizer.patterns.tunnels.TunnelShape;
 
 public class McMiningOptimizer {
 
     private static final int ITERATIONS = 8000;
 
     public static void main(String[] args) {
-
         Chunk baseChunk = new Chunk(16, 16, 16);
         OreGenerator oreGenerator = new OreGenerator();
-        DiggingPattern pattern = new DigEverythingPattern();
 
+        DiggingPattern digEverythingPattern = new DigEverythingPattern();
+        System.out.println("DIG EVERYTHING");
+        printStats(digEverythingPattern, oreGenerator, baseChunk);
+
+        DiggingPattern branchPattern2 = new BranchingPattern(TunnelPattern.STANDARD_SHAFT, TunnelPattern.STANDARD_BRANCH_2SPACED, 11, 2);
+        System.out.println("STANDARD BRANCHING - 2 spaced");
+        printStats(branchPattern2, oreGenerator, baseChunk);
+
+        DiggingPattern branchPattern3 = new BranchingPattern(TunnelPattern.STANDARD_SHAFT, TunnelPattern.STANDARD_BRANCH_3SPACED, 11, 2);
+        System.out.println("STANDARD BRANCHING - 3 spaced");
+        printStats(branchPattern3, oreGenerator, baseChunk);
+    }
+
+    private static void printStats(DiggingPattern pattern, OreGenerator oreGenerator, Chunk testChunk) {
         long totalOres = 0;
         long foundOres = 0;
         long dugBlocks = 0;
@@ -22,7 +37,7 @@ public class McMiningOptimizer {
         long startTime = System.currentTimeMillis();
 
         for (int i = 0; i < ITERATIONS; i++) {
-            Chunk oredChunk = oreGenerator.generate(baseChunk, 5);
+            Chunk oredChunk = oreGenerator.generate(testChunk, 5);
             long initialOres = oredChunk.getOresCount();
             totalOres += initialOres;
 
@@ -44,10 +59,15 @@ public class McMiningOptimizer {
         System.out.format("Found ores: %10.2f  %,12d%n", avgFoundOres, foundOres);
         System.out.format("Dug Blocks: %10.2f  %,12d%n", avgDugBlocks, dugBlocks);
         System.out.println();
-        System.out.format("Efficiency:    %6.2f%%%n", efficiency);
-        System.out.format("Thoroughness:  %6.2f%%%n", thoroughness);
+        if (dugBlocks == 0) {
+            System.out.format("/!\\ This pattern didn't dig anything!%n");
+        } else {
+            System.out.format("Efficiency:    %6.2f%%%n", efficiency);
+            System.out.format("Thoroughness:  %6.2f%%%n", thoroughness);
+        }
         System.out.println();
-        System.out.format("Exec. time: %d ms", endTime - startTime);
+        System.out.format("Exec. time: %d ms%n", endTime - startTime);
+        System.out.println();
     }
 
 }
