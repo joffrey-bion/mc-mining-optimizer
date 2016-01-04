969b41c57e6419924d69f6bfc4fe20b0817433ff
Joffrey BION <joffrey.bion@gmail.com>
2016-01-05 00:02:17 +0100

    extracted stats calculations from main

--- src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
+++ src/main/java/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.java
@@ -15,9 +15,9 @@ import java.lang.management.ThreadMXBean;
  */
 public class McMiningOptimizer {
 
-    private static final boolean VISUAL_DEBUG = true;
+    private static final boolean VISUAL_DEBUG = false;
 
-    private static final int ITERATIONS = 10000;
+    private static final int NB_ITERATIONS = 10000;
 
     private static final int SAMPLE_HEIGHT = 10;
 
@@ -28,78 +28,43 @@ public class McMiningOptimizer {
     private static final long NANOSECONDS_IN_A_MILLI = 1_000_000L;
 
     public static void main(String... args) {
-        Sample baseSample = new Sample(Sample.CHUNK_WIDTH, SAMPLE_HEIGHT, Sample.CHUNK_LENGTH);
+        Sample reference = new Sample(Sample.CHUNK_WIDTH, SAMPLE_HEIGHT, Sample.CHUNK_LENGTH);
         OreGenerator oreGenerator = new OreGenerator();
 
         DiggingPattern digEverythingPattern = new DigEverythingPattern();
         System.out.println("DIG EVERYTHING");
-        printStats(digEverythingPattern, oreGenerator, baseSample);
+        printStats(digEverythingPattern, oreGenerator, reference);
 
         DiggingPattern branchPattern2 =
                 new BranchingPattern(TunnelPattern.STANDARD_SHAFT, TunnelPattern.STANDARD_BRANCH_2SPACED, BRANCH_LENGTH,
                         BRANCH_OFFSET);
         System.out.println("STANDARD BRANCHING - 2 spaced");
-        printStats(branchPattern2, oreGenerator, baseSample);
+        printStats(branchPattern2, oreGenerator, reference);
 
         DiggingPattern branchPattern3 =
                 new BranchingPattern(TunnelPattern.STANDARD_SHAFT, TunnelPattern.STANDARD_BRANCH_3SPACED, BRANCH_LENGTH,
                         BRANCH_OFFSET);
         System.out.println("STANDARD BRANCHING - 3 spaced");
-        printStats(branchPattern3, oreGenerator, baseSample);
+        printStats(branchPattern3, oreGenerator, reference);
 
         DiggingPattern branchPatternHighShaft3 =
                 new BranchingPattern(TunnelPattern.BIG_SHAFT, TunnelPattern.STANDARD_BRANCH_3SPACED, BRANCH_LENGTH,
                         BRANCH_OFFSET);
         System.out.println("STANDARD BRANCHING (HIGH SHAFT) - 3 spaced");
-        printStats(branchPatternHighShaft3, oreGenerator, baseSample);
+        printStats(branchPatternHighShaft3, oreGenerator, reference);
     }
 
-    private static void printStats(DiggingPattern pattern, OreGenerator oreGenerator, Sample testSample) {
-        final int NB_SAMPLES = VISUAL_DEBUG ? 1 : ITERATIONS;
-
-        long totalOres = 0;
-        long foundOres = 0;
-        long dugBlocks = 0;
-
+    private static void printStats(DiggingPattern pattern, OreGenerator oreGenerator, Sample reference) {
         ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
         long startTime = threadMXBean.getCurrentThreadCpuTime();
 
-        for (int i = 0; i < NB_SAMPLES; i++) {
-            Sample sample = oreGenerator.generate(testSample, 5);
-            long initialOres = sample.getOresCount();
-            totalOres += initialOres;
-            if (VISUAL_DEBUG) {
-                System.out.println(sample);
-            }
-
-            pattern.dig(sample);
-            dugBlocks += sample.getDugBlocksCount();
-            foundOres += initialOres - sample.getOresCount();
-            if (VISUAL_DEBUG) {
-                System.out.println(sample);
-            }
-        }
+        final int n = VISUAL_DEBUG ? 1 : NB_ITERATIONS;
+        Statistics stats = Statistics.evaluate(pattern, oreGenerator, reference, n, VISUAL_DEBUG);
 
         long endTime = threadMXBean.getCurrentThreadCpuTime();
 
-        double avgTotalOres = (double) totalOres / NB_SAMPLES;
-        double avgFoundOres = (double) foundOres / NB_SAMPLES;
-        double avgDugBlocks = (double) dugBlocks / NB_SAMPLES;
-        double efficiency = (double) foundOres * 100 / dugBlocks;
-        double thoroughness = (double) foundOres * 100 / totalOres;
+        System.out.println(stats);
 
-        System.out.format("            %10s  %12s%n", "Avg/sample", "Total");
-        System.out.format("Total ores: %10.2f  %,12d%n", avgTotalOres, totalOres);
-        System.out.format("Found ores: %10.2f  %,12d%n", avgFoundOres, foundOres);
-        System.out.format("Dug Blocks: %10.2f  %,12d%n", avgDugBlocks, dugBlocks);
-        System.out.println();
-        if (dugBlocks == 0) {
-            System.out.format("/!\\ This pattern didn't dig anything!%n");
-        } else {
-            System.out.format("Efficiency:    %6.2f%%%n", efficiency);
-            System.out.format("Thoroughness:  %6.2f%%%n", thoroughness);
-        }
-        System.out.println();
         long execTimeMillis = (endTime - startTime) / NANOSECONDS_IN_A_MILLI;
         System.out.format("Execution time: %.3fs%n", (double) execTimeMillis / 1000);
         System.out.println();
---
+++ src/main/java/org/hildan/minecraft/mining/optimizer/Statistics.java
@@ -0,0 +1,73 @@
+package org.hildan.minecraft.mining.optimizer;
+
+import org.hildan.minecraft.mining.optimizer.chunks.Sample;
+import org.hildan.minecraft.mining.optimizer.ore.OreGenerator;
+import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern;
+
+/**
+ * Represents statistics about a digging pattern.
+ */
+public class Statistics {
+
+    private final int nbSamples;
+
+    private long totalOres = 0;
+
+    private long foundOres = 0;
+
+    private long dugBlocks = 0;
+
+    public Statistics(int nbSamples) {
+        this.nbSamples = nbSamples;
+    }
+
+    public static Statistics evaluate(DiggingPattern pattern, OreGenerator oreGenerator, Sample reference,
+                                      int nbSamples, boolean debug) {
+        Statistics stats = new Statistics(nbSamples);
+        for (int i = 0; i < nbSamples; i++) {
+            Sample sample = oreGenerator.generate(reference, 5);
+            long initialOres = sample.getOresCount();
+            stats.totalOres += initialOres;
+            if (debug) {
+                System.out.println(sample);
+            }
+
+            pattern.dig(sample);
+            stats.dugBlocks += sample.getDugBlocksCount();
+            stats.foundOres += initialOres - sample.getOresCount();
+            if (debug) {
+                System.out.println(sample);
+            }
+        }
+        return stats;
+    }
+
+    public double getEfficiency() {
+        return (double) foundOres * 100 / dugBlocks;
+    }
+
+    public double getThoroughness() {
+        return (double) foundOres * 100 / totalOres;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        double avgTotalOres = (double) totalOres / nbSamples;
+        double avgFoundOres = (double) foundOres / nbSamples;
+        double avgDugBlocks = (double) dugBlocks / nbSamples;
+
+        sb.append(String.format("            %10s  %12s%n", "Avg/sample", "Total"));
+        sb.append(String.format("Total ores: %10.2f  %,12d%n", avgTotalOres, totalOres));
+        sb.append(String.format("Found ores: %10.2f  %,12d%n", avgFoundOres, foundOres));
+        sb.append(String.format("Dug Blocks: %10.2f  %,12d%n", avgDugBlocks, dugBlocks));
+        sb.append(String.format("%n"));
+        if (dugBlocks == 0) {
+            sb.append(String.format("/!\\ The pattern didn't dig anything!%n"));
+        } else {
+            sb.append(String.format("Efficiency:    %6.2f%%%n", getEfficiency()));
+            sb.append(String.format("Thoroughness:  %6.2f%%%n", getThoroughness()));
+        }
+        return sb.toString();
+    }
+}
