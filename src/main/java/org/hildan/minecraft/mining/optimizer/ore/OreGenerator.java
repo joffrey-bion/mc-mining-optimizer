f9f080d5712b1d75d6c9ebac2c7be5142c449898
Joffrey BION <joffrey.bion@gmail.com>
2016-01-11 02:48:18 +0100

    fixed ore generation for samples bigger than a chunk in X and Z dimensions

--- src/main/java/org/hildan/minecraft/mining/optimizer/ore/OreGenerator.java
+++ src/main/java/org/hildan/minecraft/mining/optimizer/ore/OreGenerator.java
@@ -8,6 +8,16 @@ import java.util.Random;
 
 public class OreGenerator {
 
+    /**
+     * The length of a standard Minecraft chunk.
+     */
+    private static final int CHUNK_LENGTH = 16;
+
+    /**
+     * The width of a standard Minecraft chunk.
+     */
+    private static final int CHUNK_WIDTH = 16;
+
     private final Random random = new Random();
 
     private int lowestY;
@@ -35,43 +45,53 @@ public class OreGenerator {
         this.lowestY = sampleYPosition;
 
         Sample sample = new Sample(baseSample);
-        genStandardOre(sample, BlockType.COAL_ORE);
-        genStandardOre(sample, BlockType.IRON_ORE);
-        genStandardOre(sample, BlockType.GOLD_ORE);
-        genStandardOre(sample, BlockType.REDSTONE_ORE);
-        genStandardOre(sample, BlockType.DIAMOND_ORE);
-        genLayeredOre(sample, BlockType.LAPIS_ORE);
+        for (int x = 0; x < sample.getWidth(); x += CHUNK_WIDTH) {
+            for (int z = 0; z < sample.getLength(); z += CHUNK_LENGTH) {
+                genStandardOre(sample, BlockType.COAL_ORE, x, z);
+                genStandardOre(sample, BlockType.IRON_ORE, x, z);
+                genStandardOre(sample, BlockType.GOLD_ORE, x, z);
+                genStandardOre(sample, BlockType.REDSTONE_ORE, x, z);
+                genStandardOre(sample, BlockType.DIAMOND_ORE, x, z);
+                genLayeredOre(sample, BlockType.LAPIS_ORE, x, z);
+            }
+        }
         return sample;
     }
 
-    private void genStandardOre(Sample sample, BlockType type) {
+    private void genStandardOre(Sample sample, BlockType type, int xOffset, int zOffset) {
         WorldGenMinable gen = oreGenerators.get(type);
         int minY = type.getMinYAvailability();
         int maxY = type.getMaxYAvailability();
         for (int i = 0; i < type.getVeinsCountPerChunk(); ++i) {
-            int x = random.nextInt(sample.getWidth());
+            int x = random.nextInt(CHUNK_WIDTH);
             int y = random.nextInt(maxY - minY) + minY;
-            int z = random.nextInt(sample.getLength());
+            int z = random.nextInt(CHUNK_LENGTH);
 
-            // Y is generated within a chunk, we check whether it falls in our sample, which is a slice of the chunk
+            // coords are generated within a chunk, we check whether it falls in our sample, which is part of a chunk
             // this gives us the same probability as if the sample was actually taken from the Minecraft-generated world
-            if (lowestY <= y && y < lowestY + sample.getHeight()) {
-                gen.generateInto(sample, random, x, y - lowestY, z);
+            if (x + xOffset < sample.getWidth() && z + zOffset < sample.getLength()) {
+                if (lowestY <= y && y < lowestY + sample.getHeight()) {
+                    gen.generateInto(sample, random, x + xOffset, y - lowestY, z + zOffset);
+                }
             }
         }
     }
 
-    private void genLayeredOre(Sample sample, BlockType type) {
+    private void genLayeredOre(Sample sample, BlockType type, int xOffset, int zOffset) {
         WorldGenMinable gen = oreGenerators.get(type);
         int minY = type.getMinYAvailability();
         int maxY = type.getMaxYAvailability();
         for (int i = 0; i < type.getVeinsCountPerChunk(); ++i) {
-            int x = random.nextInt(sample.getWidth());
+            int x = random.nextInt(CHUNK_WIDTH);
             int y = 2 * random.nextInt(maxY) + (minY - maxY);
-            int z = random.nextInt(sample.getLength());
+            int z = random.nextInt(CHUNK_LENGTH);
 
-            if (lowestY <= y && y < lowestY + sample.getHeight()) {
-                gen.generateInto(sample, random, x, y - lowestY, z);
+            // coords are generated within a chunk, we check whether it falls in our sample, which is part of a chunk
+            // this gives us the same probability as if the sample was actually taken from the Minecraft-generated world
+            if (x + xOffset < sample.getWidth() && z + zOffset < sample.getLength()) {
+                if (lowestY <= y && y < lowestY + sample.getHeight()) {
+                    gen.generateInto(sample, random, x + xOffset, y - lowestY, z + zOffset);
+                }
             }
         }
     }
