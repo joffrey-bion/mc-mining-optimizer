e0dc036b820f87d1ecc35a4fb2eb967e4ec83b09
Joffrey BION <joffrey.bion@gmail.com>
2016-01-24 16:37:48 +0100

    added central Access for pattern generation

--- src/main/java/org/hildan/minecraft/mining/optimizer/patterns/generated/PatternGenerator.java
+++ src/main/java/org/hildan/minecraft/mining/optimizer/patterns/generated/PatternGenerator.java
@@ -1,8 +1,11 @@
 package org.hildan.minecraft.mining.optimizer.patterns.generated;
 
 import org.hildan.minecraft.mining.optimizer.chunks.Sample;
+import org.hildan.minecraft.mining.optimizer.patterns.Access;
 import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 /**
@@ -21,7 +24,9 @@ public class PatternGenerator implements Iterable<DiggingPattern> {
 
     @Override
     public Iterator<DiggingPattern> iterator() {
-        // FIXME generate accesses?
-        return new PatternIterator(base, null, constraints);
+        Collection<Access> accesses = new ArrayList<>();
+        // TODO generate more accesses
+        accesses.add(new Access(base.getWidth() / 2, base.getHeight() / 2));
+        return new PatternIterator(base, accesses, constraints);
     }
 }
