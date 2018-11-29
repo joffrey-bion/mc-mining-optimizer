36845fe2dedbfc3c495fdc61513c66dee74a34bd
jbion <joffrey.bion@amadeus.com>
2018-11-29 15:02:46 +0100

    Move gradle build script to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.10"
+    application
+}
+
+group = "org.hildan"
+version = "0.1"
+
+application {
+    mainClassName = "org.hildan.minecraft.mining.optimizer.McMiningOptimizerKt"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+    testCompile("org.jetbrains.kotlin:kotlin-test-junit5")
+}
+
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
