f4c8f6f4241b8112f13654139e2e1173b6e67184
jbion <joffrey.bion@amadeus.com>
2018-12-04 14:57:18 +0100

    Improve GeneratedPattern.digInto() perf by using for loop on array

--- src/main/kotlin/org/hildan/minecraft/mining/optimizer/patterns/generated/GeneratedPattern.kt
+++ src/main/kotlin/org/hildan/minecraft/mining/optimizer/patterns/generated/GeneratedPattern.kt
@@ -1,14 +1,20 @@
 package org.hildan.minecraft.mining.optimizer.patterns.generated
 
 import org.hildan.minecraft.mining.optimizer.blocks.Sample
-import org.hildan.minecraft.mining.optimizer.geometry.BlockIndex
+import org.hildan.minecraft.mining.optimizer.geometry.BlockIndices
 import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern
 
 /**
  * A pattern that can be programmatically generated.
  */
 internal class GeneratedPattern(
-    private val dugPositions: Set<BlockIndex>
+    private val dugPositions: BlockIndices
 ) : DiggingPattern {
-    override fun digInto(sample: Sample) = dugPositions.forEach { sample.digBlock(it) }
+
+    override fun digInto(sample: Sample) {
+        // using for loop because way faster than forEach() and we repeat that a lot
+        for (it in dugPositions) {
+            sample.digBlock(it)
+        }
+    }
 }
