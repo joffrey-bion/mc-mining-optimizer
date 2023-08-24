065ea1c3068d88d6b5abe39150f7866b100b0384
Joffrey Bion <joffrey.bion@gmail.com>
2023-08-24 20:25:32 +0200

    Upgrade coroutines to 1.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation(kotlin("test-junit5"))
 }
