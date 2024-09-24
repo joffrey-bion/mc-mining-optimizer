d5eaebeda7783f8ac83ffd0426ccda1b811f7893
Joffrey Bion <joffrey.bion@gmail.com>
2024-09-24 03:38:01 +0200

    Use suspend main() and remove runBlocking

--- src/main/kotlin/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.kt
+++ src/main/kotlin/org/hildan/minecraft/mining/optimizer/McMiningOptimizer.kt
@@ -1,22 +1,13 @@
 package org.hildan.minecraft.mining.optimizer
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.hildan.minecraft.mining.optimizer.blocks.Sample
-import org.hildan.minecraft.mining.optimizer.geometry.Dimensions
-import org.hildan.minecraft.mining.optimizer.ore.BlockType
-import org.hildan.minecraft.mining.optimizer.ore.generateSamples
-import org.hildan.minecraft.mining.optimizer.patterns.DiggingPattern
-import org.hildan.minecraft.mining.optimizer.patterns.generated.GenerationConstraints
-import org.hildan.minecraft.mining.optimizer.patterns.generated.PatternGenerator
-import org.hildan.minecraft.mining.optimizer.statistics.EvaluatedPattern
-import org.hildan.minecraft.mining.optimizer.statistics.PatternEvaluator
-import org.hildan.minecraft.mining.optimizer.statistics.PatternStore
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import org.hildan.minecraft.mining.optimizer.blocks.*
+import org.hildan.minecraft.mining.optimizer.geometry.*
+import org.hildan.minecraft.mining.optimizer.ore.*
+import org.hildan.minecraft.mining.optimizer.patterns.*
+import org.hildan.minecraft.mining.optimizer.patterns.generated.*
+import org.hildan.minecraft.mining.optimizer.statistics.*
 
 private const val NUM_EVAL_SAMPLES = 50
 
@@ -28,7 +19,7 @@ private const val SAMPLE_LOW_Y_POSITION = 5
 
 private const val MAX_DUG_BLOCKS = 20
 
-fun main() = runBlocking {
+suspend fun main() = withContext(Dispatchers.Default) {
     val sampleDimensions = Dimensions(SAMPLE_WIDTH, SAMPLE_HEIGHT, SAMPLE_LENGTH)
     val constraints = GenerationConstraints(sampleDimensions, MAX_DUG_BLOCKS)
 
